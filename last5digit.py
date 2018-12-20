561e62635c41a79ce55d0041a8a261e30781f824
mycode01 <mycode01@hotmail.com>
2018-12-20 09:14:25 +0900

    Update last5digit.py

--- last5digit.py
+++ last5digit.py
@@ -1,8 +1,9 @@
-def getLastDigit(inp, l=5):
+def getLastDigit(inp1, inp2, l=5):
+    inp = inp1 ** inp2
     if inp > 9999 :
         return int(str(inp)[-5:])
     else :
         return inp
 
-print(getLastDigit(12345678901247))
-print(getLastDigit(123))
+print(getLastDigit(2,26))
+print(getLastDigit(123456789,12345))
